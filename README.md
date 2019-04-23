897181b082dd67fcc456c0e17a035101abde1716
Danex2 <khadane.miller@gmail.com>
2019-04-23 17:36:36 -0400

    commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Svelte todo
+
+How else are you going to play around with something new?
+
+## Run
+
+`cd svelte-todo`  
+`yarn`
+`yarn dev`  
+Navigate to `localhost:8080`
+
+## Initial thoughts
+
+I like it, much easier for me to get started compared to all of the other frameworks. Still need to poke around the docs and do some more reading to make sure I understand what's going on but I'll probably make some more projects with this in the near future.
---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	let todos = [];
+	let todo = "";
+
+	function addTodo() {
+	  todos = [
+	    ...todos,
+	    {
+	      id: Math.random(),
+	      text: todo
+	    }
+	  ];
+	  todo = "";
+	}
+
+	function clearTodos() {
+	  todos = [];
+	}
+</script>
+
+<style>
+</style>
+
+<h1>Todo app made with Svelte!</h1>
+<form on:submit|preventDefault={addTodo}>
+<input type="text" bind:value={todo} placeholder="Add todo!">
+<button>Add</button>
+</form>
+<button on:click={clearTodos}>Clear</button>
+<ul>
+{#each todos as todo}
+	<li>{todo.text}</li>
+{/each}
+</ul>
